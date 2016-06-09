c740e4c5b51972f535694efadc32e38c9981a454
Willians <Willians@DESKTOP-9RR1TOK>
2016-06-08 22:44:26 -0300

    Alterção no CardapioItem

--- Vemkapp/Database/CardapioItem.sql
+++ Vemkapp/Database/CardapioItem.sql
@@ -3,7 +3,7 @@
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
     [CardapioId] INT NOT NULL, 
     [ItemId] INT NOT NULL, 
-    [Quantidade] DECIMAL(12, 2) NOT NULL, 
+    [Quantidade] DECIMAL(12, 2) NULL, 
     CONSTRAINT [FK_CardapioItem_Cardapio] FOREIGN KEY (CardapioId) REFERENCES Cardapio(Id), 
     CONSTRAINT [FK_CardapioItem_Item] FOREIGN KEY (ItemId) REFERENCES Item(Id)
 )
